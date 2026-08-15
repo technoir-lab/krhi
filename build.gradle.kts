d4fc7f0dbaa470c1d1ec866d428fa4ae25529012
technoir <technoir.dev@gmail.com>
2026-08-15 15:06:03 +0100

    Sort dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,13 +3,13 @@ plugins {
 }
 
 dependencies {
-    dokka(project(":core"))
     dokka(project(":backend:mock"))
     dokka(project(":backend:vulkan"))
     dokka(project(":backend:webgpu"))
+    dokka(project(":core"))
 
-    nmcpAggregation(project(":core"))
     nmcpAggregation(project(":backend:mock"))
     nmcpAggregation(project(":backend:vulkan"))
     nmcpAggregation(project(":backend:webgpu"))
+    nmcpAggregation(project(":core"))
 }
